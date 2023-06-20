1b7b3cf847f228c0e472049c92c11d75a0d4ab9d
Shirahamah <hiropi1224s@gmail.com>
2023-06-21 00:11:43 +0900

    ✅ add 3060

---
+++ src/types/easy/3060/index.ts
@@ -0,0 +1,36 @@
+/*
+  3060 - Unshift
+  -------
+  by jiangshan (@jiangshanmeta) #初級 #array
+
+  ### 質問
+
+  ```Array.unshift```の型バージョンを実装します。
+
+  例えば：
+
+  ```typescript
+  type Result = Unshift<[1, 2], 0> // [0, 1, 2,]
+  ```
+
+  > GitHubで確認する：https://tsch.js.org/3060/ja
+*/
+
+/* _____________ ここにコードを記入 _____________ */
+
+type Unshift<T extends unknown[], U> = [U, ...T];
+
+/* _____________ テストケース _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Unshift<[], 1>, [1]>>,
+  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
+  Expect<Equal<Unshift<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>
+];
+
+/* _____________ 解説 _____________ */
+/*
+ 既存配列型の1番目に受け取った値を追加すればよい。
+ Tは配列型のためextends unknown[]とし、[U, ...T]とする。
+*/
